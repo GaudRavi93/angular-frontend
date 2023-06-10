c104bb7ff8f30df75be4999d37fb82da46866793
GaudRavi <ravi.disolutions@gmail.com>
2023-06-10 11:56:32 +0530

    add missing files

---
+++ src/app/shared/components/sidebar/app.sidebar.component.ts
@@ -0,0 +1,54 @@
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { LayoutService } from '../../services/app.layout.service';
+
+@Component({
+    selector: 'app-sidebar',
+    templateUrl: './app.sidebar.component.html'
+})
+export class AppSidebarComponent implements OnInit {
+    model: any[] = [];
+    constructor(public layoutService: LayoutService, public el: ElementRef) { }
+    ngOnInit() {
+        this.model = [
+            {
+                label: 'Home',
+                items: [
+                    { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/layout/dashboard'] }
+                ]
+            },
+            {
+                label: 'Pages',
+                icon: 'pi pi-fw pi-briefcase',
+                items: [
+                    {
+                        label: 'Auth',
+                        icon: 'pi pi-fw pi-user',
+                        items: [
+                            {
+                                label: 'Login',
+                                icon: 'pi pi-fw pi-sign-in',
+                                routerLink: ['/login']
+                            },
+                            {
+                                label: 'Error',
+                                icon: 'pi pi-fw pi-times-circle',
+                                routerLink: ['/error']
+                            },
+                            {
+                                label: 'Access Denied',
+                                icon: 'pi pi-fw pi-lock',
+                                routerLink: ['/access']
+                            }
+                        ]
+                    },
+                    {
+                        label: 'Not Found',
+                        icon: 'pi pi-fw pi-exclamation-circle',
+                        routerLink: ['/notfound']
+                    },
+                ]
+            }
+        ];
+    }
+}
+
