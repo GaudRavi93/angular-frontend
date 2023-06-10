7c415220bbddc930cad182d32649e2ff4b498e0d
GaudRavi <ravi.disolutions@gmail.com>
2023-06-10 10:17:10 +0530

    add dark mode

--- src/app/shared/components/layout/app.topbar.component.ts
+++ src/app/shared/components/layout/app.topbar.component.ts
@@ -11,6 +11,8 @@ export class AppTopBarComponent {
 
     items!: MenuItem[];
 
+    isDarkMode: boolean = false;
+
     @ViewChild('menubutton') menuButton!: ElementRef;
 
     @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef;
@@ -21,4 +23,36 @@ export class AppTopBarComponent {
         public layoutService: LayoutService,
         public authService: AuthService,
     ) { }
+
+    toggleTheme() {
+        this.isDarkMode = !this.isDarkMode;
+        const theme = this.isDarkMode ? 'lara-dark-indigo' : 'lara-light-indigo';
+        const colorScheme = this.isDarkMode ? 'dark' : 'light';
+        const themeLink = <HTMLLinkElement>document.getElementById('theme-css');
+        const newHref = themeLink.getAttribute('href')!.replace(this.layoutService.config.theme, theme);
+
+        this.layoutService.config.colorScheme;
+        this.replaceThemeLink(newHref, () => {
+            this.layoutService.config.theme = theme;
+            this.layoutService.config.colorScheme = colorScheme;
+            this.layoutService.onConfigUpdate();
+        });
+    }
+
+    replaceThemeLink(href: string, onComplete: Function) {
+        const id = 'theme-css';
+        const themeLink = <HTMLLinkElement>document.getElementById('theme-css');
+        const cloneLinkElement = <HTMLLinkElement>themeLink.cloneNode(true);
+
+        cloneLinkElement.setAttribute('href', href);
+        cloneLinkElement.setAttribute('id', id + '-clone');
+
+        themeLink.parentNode!.insertBefore(cloneLinkElement, themeLink.nextSibling);
+
+        cloneLinkElement.addEventListener('load', () => {
+            themeLink.remove();
+            cloneLinkElement.setAttribute('id', id);
+            onComplete();
+        });
+    }
 }
